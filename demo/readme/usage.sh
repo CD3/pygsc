7e8f3a25a882afa061adc7f2c70ac29bc66c187f
CD Clark III <clifton.clark@gmail.com>
2020-11-28 13:55:10 -0600

    docs: adding demo script for readme

---
+++ demo/readme/usage.sh
@@ -0,0 +1,14 @@
+echo "characters are sent to the shell one at a time as you type. to load a new line, press return."
+ls
+vim demo.txt
+
+aterminal applications like vim, gnuplot, or remote ssh connections can all be scripted
+
+:wq
+ls
+cat demo.txt
+gnuplot
+plot sin(x)
+exit
+echo "happy demoing!"
+
